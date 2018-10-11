e5a72aed5cea2d9e506fc157adfc7363a81e8a5a
yuta.hirata <engineer.yuta.hirata@gmail.com>
2018-10-11 09:14:01 +0000

    post

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   
   before_save{ self.email.downcase!}
   
+  has_many :microposts
+  
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255},
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
