4862bbd96a5a95930d3ec04165f9fee0a7b264fd
yuta.hirata <engineer.yuta.hirata@gmail.com>
2018-10-16 08:07:15 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
   
   def index
     @users = User.all.page(params[:page])
@@ -17,13 +17,25 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.create
+    if @user.save
       flash[:success] = 'ユーザを登録しました'
       redirect_to @user
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました'
       render :new
     end
+  end
+  
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
   end
   
   private
